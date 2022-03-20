a788107a3871e435d30d036d8658597542dca910
root <root@homeassistant.local.hass.io>
2022-03-20 10:17:31 -0400

    Apply Black formatting to color_swarm

--- color_swarm.py
+++ color_swarm.py
@@ -24,8 +24,8 @@ swarms = {
             },
             {
                 "rgb_color": (0, 255, 0),
-                "brightness": 100,                
-            }
+                "brightness": 100,
+            },
         ],
     },
     "Bright Christmas": {
@@ -206,16 +206,16 @@ swarms = {
                 # Gold
                 "rgb_color": (255, 205, 49),
                 "brightness": 240,
-            }
+            },
         ]
-        +
-        [
+        + [
             {
                 # White
                 "kelvin": 3200,
                 "brightness": 255,
             },
-        ] * 10,
+        ]
+        * 10,
     },
     "USA": {
         "transition_secs": 3,
@@ -262,7 +262,7 @@ swarms = {
             {
                 "rgb_color": (1, 82, 104),
                 "brightness": 255,
-            },            
+            },
         ],
     },
     "Summer night": {
@@ -278,7 +278,7 @@ swarms = {
                 "brightness": 1,
             },
         ],
-    },        
+    },
     "Candlelight": {
         "transition_secs": 0.25,
         "max_hold_secs": 4,
@@ -308,17 +308,17 @@ swarms = {
                 "brightness": 70,
             },
         ],
-    },        
+    },
 }
 
 
 def light_entities_for_group(group_name):
     """Find light entity IDs for the Philips Hue group/room name.
-    
+
     All configured Hue bridges are queried for the group, since HA doesn't store Hue group membership data.
     Pyscript must be configured to expose the "hass" global variable and allow all imports so that we can
     access the bridge configs and entity registry.
-    
+
     :param group_name: The Hue group/room name exactly as it appears in the Hue app (e.g. "Living room").
     :return: List of light entity IDs for the group name or empty list if no matching group or entities are found.
     """
@@ -331,8 +331,11 @@ def light_entities_for_group(group_name):
         host, api_key = config_entry.data["host"], config_entry.data["api_key"]
         async with HueBridgeV2(host, api_key) as bridge:
             # Query Hue bridge for lights in the matching group (if any).
-            local_light_id_groups = [group.lights for group in bridge.groups 
-                                    if hasattr(group, 'metadata') and group.metadata.name == group_name]
+            local_light_id_groups = [
+                group.lights
+                for group in bridge.groups
+                if hasattr(group, "metadata") and group.metadata.name == group_name
+            ]
             if not local_light_id_groups:
                 continue
             local_light_ids = [id for id_group in local_light_id_groups for id in id_group]
@@ -341,31 +344,36 @@ def light_entities_for_group(group_name):
             # Get unique IDs for the lights.
             unique_ids = {bridge.lights[id].id for id in local_light_ids}
             # Get entity IDs for unique IDs.
-            entity_ids += [id for id, entity in entity_registry.entities.items() 
-                            if entity.unique_id in unique_ids and entity.platform == "hue"]
+            entity_ids += [
+                id
+                for id, entity in entity_registry.entities.items()
+                if entity.unique_id in unique_ids and entity.platform == "hue"
+            ]
             log.debug(f"Entites added to group {group_name}: {entity_ids}")
     return entity_ids
 
 
 @service
-def color_swarm_turn_on(hue_group_name = "Office", swarm_name = "Christmas"):
+def color_swarm_turn_on(hue_group_name="Office", swarm_name="Christmas"):
     """Start the color swarm effect on the specified Philips Hue light group.
-    
+
     The color swarm comtinues running on the group until it is turned off or turned on with different parameters.
-    
+
     :param hue_group_name: Name of the Hue light group or room, exactly as it appears in the Hue app. Case-sensitive.
     :param swarm_name: The predefined swarm definition including color palette and transitions.
     """
-    
+
     if swarm_name not in swarms:
         raise ValueError(f"Swarm '{swarm_name}' does not exist.")
     task.unique(f"color-swarm-{hue_group_name}")
     entity_ids = light_entities_for_group(hue_group_name)
     if entity_ids:
-        log.info(f"Started '{swarm_name}' color swarm for Hue group '{hue_group_name}' consisting of {len(entity_ids)} light(s).")
+        log.info(
+            f"Started '{swarm_name}' color swarm for Hue group '{hue_group_name}' consisting of {len(entity_ids)} light(s)."
+        )
     else:
         log.error(f"No light entities found for Hue group '{hue_group_name}'.")
-    
+
     # Create a priority queue of the next transition per light, sorted by random future transition times.
     swarm = swarms[swarm_name]
     transition_q = []
@@ -374,7 +382,7 @@ def color_swarm_turn_on(hue_group_name = "Office", swarm_name = "Christmas"):
         change_time = random.uniform(start_time, start_time + swarm["max_hold_secs"])
         change_color = random.choice(swarm["palette"])
         heapq.heappush(transition_q, (change_time, entity_id, change_color))
-    
+
     # This will loop forever as long as there are lights and the task isn't killed.
     while transition_q:
         head_time, entity_id, head_color = heapq.heappop(transition_q)
@@ -391,10 +399,10 @@ def color_swarm_turn_on(hue_group_name = "Office", swarm_name = "Christmas"):
         now = time.monotonic()
         next_time = swarm["transition_secs"] + random.uniform(now, now + swarm["max_hold_secs"])
         next_color = random.choice(swarm["palette"])
-        heapq.heappush(transition_q, (next_time, entity_id, next_color))    
+        heapq.heappush(transition_q, (next_time, entity_id, next_color))
 
 
 @service
-def color_swarm_turn_off(hue_group_name = "Office"):
+def color_swarm_turn_off(hue_group_name="Office"):
     """Stop any running color swarm effect on the specified Philips Hue light group."""
     task.unique(f"color-swarm-{hue_group_name}")
